c49510ba6e3f74e6ae8498f8c0a63ae4d74d4f5c
Angelo Terzi <angeloterzi98@gmail.com>
2021-10-29 14:36:47 +0200

    :sparkles: Added functions to create new Gpx and Kml objects, as well as checks in gpx_kml.rb
    Added new_gpx and new_kml functions that take in a file path and return respectively a Gpx and a Kml object
    
    Added gpx? and kml? functions that take in a Gpx or a Kml instance and tell whether the imported file actually contained a real gpx/kml

--- lib/gpx_kml.rb
+++ lib/gpx_kml.rb
@@ -12,12 +12,28 @@ module GPXKML
   include CONVERTER
 
   class GpxKml
-    def self.gpx_to_kml(file_path, destination_path)
-      CONVERTER::Converter.gpx_to_kml(file_path, destination_path)
+    def self.gpx_to_kml(gpx, destination_path)
+      CONVERTER::Converter.gpx_to_kml(gpx, destination_path)
     end
 
-    def self.kml_to_gpx(file_path, destination_path)
-      CONVERTER::Converter.kml_to_gpx(file_path, destination_path)
+    def self.kml_to_gpx(kml, destination_path)
+      CONVERTER::Converter.kml_to_gpx(kml, destination_path)
+    end
+
+    def self.kml?(kml)
+      kml.is_a?(KML::Kml) && kml.kml?
+    end
+
+    def self.gpx?(gpx)
+      gpx.is_a?(GPX::Gpx) && gpx.gpx?
+    end
+
+    def self.new_kml(file_path)
+      KML::Kml.new(file_path)
+    end
+
+    def self.new_gpx(file_path)
+      GPX::Gpx.new(file_path)
     end
   end
 end
