3d743732caac944416053d5b66ee0942cc74dcba
Angelo Terzi <angeloterzi98@gmail.com>
2021-10-29 14:37:28 +0200

    :white_check_mark: Added test for gpx_kml.rb functions

--- spec/gpx_kml_spec.rb
+++ spec/gpx_kml_spec.rb
@@ -4,6 +4,60 @@
 
 RSpec.describe GPXKML::GpxKml do
   it 'has a version number' do
-    expect(GPXKML::VERSION).to be '0.1.2'
+    expect(GPXKML::VERSION).to be '0.1.3'
+  end
+
+  it 'checks if the imported file is actually a gpx' do
+    gpx = GPXKML::GpxKml.new_gpx("#{__dir__}/test_files/test.gpx")
+    expect(gpx.is_a?(GPX::Gpx)).to be true
+    expect(gpx.gpx?).to be true
+  end
+
+  it 'checks if the imported file is actually a kml' do
+    kml = GPXKML::GpxKml.new_kml("#{__dir__}/test_files/test.kml")
+    expect(kml.is_a?(KML::Kml)).to be true
+    expect(kml.kml?).to be true
+  end
+
+  it 'create a new, valid gpx' do
+    gpx = GPXKML::GpxKml.new_gpx("#{__dir__}/test_files/test.gpx")
+    expect(gpx.is_a?(GPX::Gpx)).to be true
+    expect(gpx.gpx?).to be true
+    expect(gpx.valid?).to be true
+  end
+
+  it 'creates a new, valid kml' do
+    kml = GPXKML::GpxKml.new_kml("#{__dir__}/test_files/test.kml")
+    expect(kml.is_a?(KML::Kml)).to be true
+    expect(kml.kml?).to be true
+    expect(kml.valid?).to be true
+  end
+
+  it 'correctly converts gpx into kml' do
+    gpx = GPXKML::GpxKml.new_gpx("#{__dir__}/test_files/test.gpx")
+    expect(gpx.is_a?(GPX::Gpx)).to be true
+    expect(gpx.gpx?).to be true
+    expect(gpx.valid?).to be true
+    filename = GPXKML::GpxKml.gpx_to_kml(gpx, "#{__dir__}/test_files")
+    f = File.open(filename)
+    f2 = File.open("#{__dir__}/test_files/preconverted_gpx.kml")
+    expect(f.read).to eq(f2.read)
+    f.close
+    f2.close
+    File.delete(filename)
+  end
+
+  it 'correctly converts kml into gpx' do
+    kml = GPXKML::GpxKml.new_kml("#{__dir__}/test_files/test.kml")
+    expect(kml.is_a?(KML::Kml)).to be true
+    expect(kml.kml?).to be true
+    expect(kml.valid?).to be true
+    filename = GPXKML::GpxKml.kml_to_gpx(kml, "#{__dir__}/test_files")
+    f = File.open(filename)
+    f2 = File.open("#{__dir__}/test_files/preconverted_kml.gpx")
+    expect(f.read).to eq(f2.read)
+    f.close
+    f2.close
+    File.delete(filename)
   end
 end
